9089bf62e848c0d24f11263e41e1240e74be2947
ahmedkhalil9 <38581927+ahmedAkramKhalil@users.noreply.github.com>
2025-06-22 10:47:39 +0300

    Push Working code

---
+++ system/addon.d/51-com.teletalker.app.sh
@@ -0,0 +1,14 @@
+#!/sbin/sh
+# ADDOND_VERSION=2
+
+. /tmp/backuptool.functions
+
+files="priv-app/com.teletalker.app/app-release.apk etc/permissions/privapp-permissions-com.teletalker.app.xml etc/sysconfig/config-com.teletalker.app.xml"
+
+case "${1}" in
+backup|restore)
+    for f in ${files}; do
+        "${1}_file" "${S}/${f}"
+    done
+    ;;
+esac
